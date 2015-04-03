00905558afd2a35ef75a898efeadb554cd08da54
Sean Moss <semo0788@colorado.edu>
2015-04-03 15:55:26 -0600

    Starting work on the testing script

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd programs
+make
+cd ..
+
+for file in results/*.res
+do
+	rm -f $file
+done
+
+echo ""
+echo ""
+echo "================================ TESTS ==============================="
+
+echo "Running CPU, small (100), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=results/cpu_s_other.res programs/cpuprog 100 SCHED_OTHER
+echo "Running CPU, small (100), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=results/cpu_s_fifo.res programs/cpuprog 100 SCHED_FIFO
+echo "Running CPU, small (100), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=results/cpu_s_rr.res programs/cpuprog 100 SCHED_RR
+
+echo "Running CPU, medium (1000), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=results/cpu_m_other.res programs/cpuprog 1000 SCHED_OTHER
+echo "Running CPU, medium (1000), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=results/cpu_m_fifo.res programs/cpuprog 1000 SCHED_FIFO
+echo "Running CPU, medium (1000), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=results/cpu_m_rr.res programs/cpuprog 1000 SCHED_RR
+
+echo "Running CPU, large (10000), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=results/cpu_l_other.res programs/cpuprog 10000 SCHED_OTHER
+echo "Running CPU, large (10000), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=results/cpu_l_fifo.res programs/cpuprog 10000 SCHED_FIFO
+echo "Running CPU, large (10000), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=results/cpu_l_rr.res programs/cpuprog 10000 SCHED_RR
+
+
+echo "======================================================================"
+echo ""
+echo ""
+
+cd programs
+make clean
+cd ..
+
+echo "Done"
