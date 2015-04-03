e8f2c682fe8b9f0dc2e658a2b6338e59b951a7a1
Sean Moss <semo0788@colorado.edu>
2015-04-03 14:26:07 -0600

    Finished initial cpu program

--- programs/cpuprog.c
+++ programs/cpuprog.c
@@ -16,12 +16,70 @@
 #include <math.h>
 #include <errno.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define NUMBER_CALCULATIONS 10000
+
+void run_calculations();
 
 int main(int argc, char** argv)
 {
+	if (argc != 2)
+	{
+		printf("Usage: \".\\cpuprog <number_of_children>\"");
+		exit(EXIT_FAILURE);
+	}
+	int numChildren = atoi(argv[1]); /* Convert to number of children to spawn. */
+	
+	printf("Spawning %d children.\n", numChildren);
+
+	/* Spawn the children processes */
+	int i;
+	for (i = 0; i < numChildren; ++i)
+	{
+		pid_t pid = fork();
+		if (pid == 0) /* Child */
+		{
+			run_calculations();
+			exit(EXIT_SUCCESS);
+		}
+	}
+
+	/* Wait for all children to finish */
+	pid_t status;
+	do
+	{
+		pid_t done = wait(&status);
+		if (done == -1 && errno != ECHILD)
+		{
+			printf("Error during wait()");
+			abort();
+		}
+	}
+	while (status > 0);
+
+	return 0;
+}
+
+void run_calculations()
+{
 	srand(time(NULL));
 	mersenne_initialize_generator(rand());
-	int r = mersenne_rand();
-	printf("The random number was: %d.\n", r);
-	return 0;
+
+	int lastx = 0;
+	int lasty = 0;
+	int i;
+	for (i = 0; i < NUMBER_CALCULATIONS; ++i)
+	{
+		int x = mersenne_rand();
+		int y = mersenne_rand();
+		int x2 = x - lastx;
+		int y2 = y - lasty;
+		x2 *= x2;
+		y2 *= y2;
+		sqrt(x2 + y2); /* Most of the CPU work will come from this call. */
+		lastx = x;
+		lasty = y;
+	}
 }
