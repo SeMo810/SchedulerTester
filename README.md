f13874ad656c192948532741768c521edb3015f4
Sean Moss <semo0788@colorado.edu>
2015-04-04 19:17:16 -0600

    Tests are now complete and finished

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SchedulerTester
 A series of programs that gathers runtime data from different scheduling processes, and generates data and graphs based on the data. It also includes my final report. This is for the CSCI 3753 class.
+
+### Structure of Repo
+* `test.sh` - Runs the tests, and generates the output files.
+* `programs` - The source code for the programs.
+* `report` - The directory where all of my final files and report is.
+
+### How to Run
+All of this should be done from the root directory.
+
+* Run `./test.sh`, this will run the tests on each program, with each count of children, and with each scheduler policy.
+  * This will output files to a `results` folder.
+* Run `python graph.py`, this will generate the final graphs and put them into the report directory.
+  * **NOTE**: You need to install plot.ly in the python libraries, using `sudo pip install plotly` for this to work.
--- test.sh
+++ test.sh
@@ -4,6 +4,14 @@ cd programs
 make
 cd ..
 
+if [ ! -d "results" ]; then
+	mkdir results
+fi
+
+if [ ! -d "programs/data" ]; then
+	mkdir programs/data
+fi
+
 for file in results/*.res
 do
 	rm -f $file
@@ -15,47 +23,47 @@ echo "================================ TESTS ==============================="
 
 cd programs
 
-#echo "===> Running CPU, small (10), SCHED_OTHER..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_s_other.res ./cpuprog 10 SCHED_OTHER
-#echo "===> Running CPU, small (10), SCHED_FIFO..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_s_fifo.res ./cpuprog 10 SCHED_FIFO
-#echo "===> Running CPU, small (10), SCHED_RR..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_s_rr.res ./cpuprog 10 SCHED_RR
+echo "===> Running CPU, small (10), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=../results/cpu_s_other.res ./cpuprog 10 SCHED_OTHER
+echo "===> Running CPU, small (10), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=../results/cpu_s_fifo.res ./cpuprog 10 SCHED_FIFO
+echo "===> Running CPU, small (10), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=../results/cpu_s_rr.res ./cpuprog 10 SCHED_RR
 
-#echo "===> Running CPU, medium (100), SCHED_OTHER..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_m_other.res ./cpuprog 100 SCHED_OTHER
-#echo "===> Running CPU, medium (100), SCHED_FIFO..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_m_fifo.res ./cpuprog 100 SCHED_FIFO
-#echo "===> Running CPU, medium (100), SCHED_RR..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_m_rr.res ./cpuprog 100 SCHED_RR
+echo "===> Running CPU, medium (100), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=../results/cpu_m_other.res ./cpuprog 100 SCHED_OTHER
+echo "===> Running CPU, medium (100), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=../results/cpu_m_fifo.res ./cpuprog 100 SCHED_FIFO
+echo "===> Running CPU, medium (100), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=../results/cpu_m_rr.res ./cpuprog 100 SCHED_RR
 
-#echo "===> Running CPU, large (1000), SCHED_OTHER..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_l_other.res ./cpuprog 1000 SCHED_OTHER
-#echo "===> Running CPU, large (1000), SCHED_FIFO..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_l_fifo.res ./cpuprog 1000 SCHED_FIFO
-#echo "===> Running CPU, large (1000), SCHED_RR..."
-#sudo /usr/bin/time --verbose --output=../results/cpu_l_rr.res ./cpuprog 1000 SCHED_RR
+echo "===> Running CPU, large (1000), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=../results/cpu_l_other.res ./cpuprog 1000 SCHED_OTHER
+echo "===> Running CPU, large (1000), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=../results/cpu_l_fifo.res ./cpuprog 1000 SCHED_FIFO
+echo "===> Running CPU, large (1000), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=../results/cpu_l_rr.res ./cpuprog 1000 SCHED_RR
 
-#echo "===> Running IO, small (10), SCHED_OTHER..."
-#sudo /usr/bin/time --verbose --output=../results/io_s_other.res ./ioprog 10 SCHED_OTHER
-#echo "===> Running IO, small (10), SCHED_FIFO..."
-#sudo /usr/bin/time --verbose --output=../results/io_s_fifo.res ./ioprog 10 SCHED_FIFO
-#echo "===> Running IO, small (10), SCHED_RR..."
-#sudo /usr/bin/time --verbose --output=../results/io_s_rr.res ./ioprog 10 SCHED_RR
+echo "===> Running IO, small (10), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=../results/io_s_other.res ./ioprog 10 SCHED_OTHER
+echo "===> Running IO, small (10), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=../results/io_s_fifo.res ./ioprog 10 SCHED_FIFO
+echo "===> Running IO, small (10), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=../results/io_s_rr.res ./ioprog 10 SCHED_RR
 
-#echo "===> Running IO, medium (100), SCHED_OTHER..."
-#sudo /usr/bin/time --verbose --output=../results/io_m_other.res ./ioprog 100 SCHED_OTHER
-#echo "===> Running IO, medium (100), SCHED_FIFO..."
-#sudo /usr/bin/time --verbose --output=../results/io_m_fifo.res ./ioprog 100 SCHED_FIFO
-#echo "===> Running IO, medium (100), SCHED_RR..."
-#sudo /usr/bin/time --verbose --output=../results/io_m_rr.res ./ioprog 100 SCHED_RR
+echo "===> Running IO, medium (100), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=../results/io_m_other.res ./ioprog 100 SCHED_OTHER
+echo "===> Running IO, medium (100), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=../results/io_m_fifo.res ./ioprog 100 SCHED_FIFO
+echo "===> Running IO, medium (100), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=../results/io_m_rr.res ./ioprog 100 SCHED_RR
 
-#echo "===> Running IO, large (1000), SCHED_OTHER..."
-#sudo /usr/bin/time --verbose --output=../results/io_l_other.res ./ioprog 1000 SCHED_OTHER
-#echo "===> Running IO, large (1000), SCHED_FIFO..."
-#sudo /usr/bin/time --verbose --output=../results/io_l_fifo.res ./ioprog 1000 SCHED_FIFO
-#echo "===> Running IO, large (1000), SCHED_RR..."
-#sudo /usr/bin/time --verbose --output=../results/io_l_rr.res ./ioprog 1000 SCHED_RR
+echo "===> Running IO, large (1000), SCHED_OTHER..."
+sudo /usr/bin/time --verbose --output=../results/io_l_other.res ./ioprog 1000 SCHED_OTHER
+echo "===> Running IO, large (1000), SCHED_FIFO..."
+sudo /usr/bin/time --verbose --output=../results/io_l_fifo.res ./ioprog 1000 SCHED_FIFO
+echo "===> Running IO, large (1000), SCHED_RR..."
+sudo /usr/bin/time --verbose --output=../results/io_l_rr.res ./ioprog 1000 SCHED_RR
 
 echo "===> Running MIX, small (10), SCHED_OTHER..."
 sudo /usr/bin/time --verbose --output=../results/mx_s_other.res ./mxprog 10 SCHED_OTHER
